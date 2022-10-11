d0241b4d19fe83857c30898abf49c43bfe6ffe3c
Gonçalo  Azevedo <zalosantos02@gmail.com>
2022-10-11 12:49:14 +0100

    random

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,18 @@
 """Forms of the project."""
 
 # Create your forms here.
+from django import forms
+from .models import Thing
+from django.core.validators import RegexValidator
+
+
+class ThingForm(forms.ModelForm):
+
+
+
+        description=forms.CharField(max_length=120, blank=True ,widget= forms.Textarea())
+        quantity=forms.IntegerField(
+            validators=[MinValueValidator(0),MaxValueValidator(50)],
+            widget= forms.NumberInput
+        )
+        name=forms.CharField(max_length=35, unique=True)
--- things/views.py
+++ things/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .forms import ThingForm
 
 def home(request):
+    if request.method == 'POST':
+        form=ThingForm(request.POST)
+        if form.is_valid():
+            thing = form.save()
+            login(request,thing)
+            return redirect('home')
+    else:
+        form= ThingForm()
     return render(request, 'home.html', {'form': form})
